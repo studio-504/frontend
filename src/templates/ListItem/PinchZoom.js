1a599f6cb5cf25c76b0df14f6c307950bfacd130
Azim Gadzhiagayev <me@azimgd.com>
2020-03-14 23:14:28 +0500

    Pinch zoom + pan support on feed list component;

--- src/templates/ListItem/PinchZoom.js
+++ src/templates/ListItem/PinchZoom.js
@@ -3,15 +3,32 @@ import PropTypes from 'prop-types'
 import {
   Animated,
 } from 'react-native'
-import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+import {
+  PinchGestureHandler,
+  PanGestureHandler,
+  State,
+} from 'react-native-gesture-handler'
 
 export class PinchZoom extends React.Component {
   baseScale = new Animated.Value(1)
   pinchScale = new Animated.Value(1)
   scale = Animated.multiply(this.baseScale, this.pinchScale)
   lastScale = 1
+
+  translateX = new Animated.Value(0)
+  translateY = new Animated.Value(0)
+  lastOffset = { x: 0, y: 0 }
+
+  panRef = React.createRef()
+  pinchRef = React.createRef()
+
   onPinchGestureEvent = Animated.event(
     [{ nativeEvent: { scale: this.pinchScale } }],
+    { useNativeDriver: false }
+  )
+
+  onPanGestureEvent = Animated.event(
+    [{ nativeEvent: { translationX: this.translateX, translationY: this.translateY }, }],
     { useNativeDriver: false }
   )
 
@@ -30,21 +47,57 @@ export class PinchZoom extends React.Component {
     }
   }
 
+  onPanHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this.lastOffset.y += event.nativeEvent.translationY
+      this.lastOffset.x += event.nativeEvent.translationX
+      this.translateX.setOffset(this.lastOffset.x)
+      this.translateX.setValue(0)
+      this.translateY.setOffset(this.lastOffset.y)
+      this.translateY.setValue(0)
+    }
+
+    if (event.nativeEvent.state === State.END) {
+      this.lastOffset.y = 0
+      this.lastOffset.x = 0
+      this.translateX.setOffset(0)
+      this.translateX.setValue(0)
+      this.translateY.setOffset(0)
+      this.translateY.setValue(0)
+    }
+  }
+
   render() {
     return (
-      <PinchGestureHandler
-        onGestureEvent={this.onPinchGestureEvent}
-        onHandlerStateChange={this.onPinchHandlerStateChange}
+      <PanGestureHandler
+        onGestureEvent={this.onPanGestureEvent}
+        onHandlerStateChange={this.onPanHandlerStateChange}
+        minPointers={2}
+        maxPointers={2}
+        ref={this.panRef}
       >
-        <Animated.View style={[this.props.style, {
-          transform: [
-            { perspective: 200 },
-            { scale: this.scale },
-          ],
-        }]}>
-          {this.props.children}
+        <Animated.View>
+          <PinchGestureHandler
+            onGestureEvent={this.onPinchGestureEvent}
+            onHandlerStateChange={this.onPinchHandlerStateChange}
+            simultaneousHandlers={this.panRef}
+            minPointers={2}
+            maxPointers={2}
+            ref={this.pinchRef}
+          >
+            <Animated.View style={[this.props.style, {
+              transform: [
+                { perspective: 200 },
+                { scale: this.scale },
+                { translateX: this.translateX },
+                { translateY: this.translateY },
+              ],
+            }]}>
+              {this.props.children}
+            </Animated.View>
+          </PinchGestureHandler>
         </Animated.View>
-      </PinchGestureHandler>
+      </PanGestureHandler>
     )
   }
 }
