0cafd6ca728120e95b2bcd7f4766ac43136c35a3
Azim Gadzhiagayev <me@azimgd.com>
2020-04-27 20:20:43 +0500

    Google authentication refactor;

--- src/services/AWS.js
+++ src/services/AWS.js
@@ -1,11 +1,7 @@
 import Auth from '@aws-amplify/auth'
 import Api from '@aws-amplify/api'
-import { LoginManager, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk'
-import { GoogleSignin } from '@react-native-community/google-signin'
 import Config from 'react-native-config'
-import * as Logger from 'services/Logger'
-import dayjs from 'dayjs'
-import promiseRetry from 'promise-retry'
+import * as Google from 'services/Google'
 
 /**
  * AWS Configuration
@@ -17,8 +13,7 @@ export const amplifyConfig = async () => {
     userPoolWebClientId: Config.AWS_COGNITO_USER_POOL_WEB_CLIENT_ID,
     identityPoolId: Config.AWS_COGNITO_IDENTITY_POOL_ID,
     refreshHandlers: {
-      'facebook': handleFacebookRefresh,
-      'google': handleGoogleRefresh,
+      'google': Google.refresh,
     },
   })
 
@@ -30,146 +25,5 @@ export const amplifyConfig = async () => {
   })
 }
 
-const getFacebookData = () => new Promise((resolve, reject) => {
-  const handleResponse = (error, result) => {
-    if (error) { reject(error) }
-    else { resolve(result) }
-  }
-  const options = { parameters: { fields: { string: 'email,name' } } }
-  const infoRequest = new GraphRequest('/me', options, handleResponse)
-  new GraphRequestManager().addRequest(infoRequest).start()
-})
-
-/**
- * 
- */
-export const federatedFacebookSignin = async () => {
-  const response = await LoginManager.logInWithPermissions(['public_profile', 'email'])
-  
-  if (response.isCancelled) {
-    throw new Error('Facebook auth cancelled')
-  }
-
-  const payload = await AccessToken.getCurrentAccessToken()
-  const facebookUser = await getFacebookData()
-  
-  return {
-    token: payload.accessToken,
-    expires_at: payload.dataAccessExpirationTime,
-    user: facebookUser,
-  }
-}
-
-const handleFacebookRefresh = async () => {
-  await LoginManager.logInWithPermissions(['public_profile', 'email'])
-  const payload = await AccessToken.getCurrentAccessToken()
-
-  return {
-    token: payload.accessToken,
-    expires_at: payload.dataAccessExpirationTime,
-  }
-}
-
-GoogleSignin.configure({
-  offlineAccess: false,
-  iosClientId: Config.GOOGLE_SIGNIN_IOS_CLIENT_ID,
-  webClientId: Config.GOOGLE_SIGNIN_ANDROID_CLIENT_ID,
-})
-
-const checkTokenExpiry = async (idToken) => {
-  const data = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`)
-  const response = await data.json()
-  
-  if (!response || response.error) {
-    throw new Error('Token expired')
-  }
-
-  return response
-}
-
-const _handleGoogleRefresh = async () => {
-  await GoogleSignin.hasPlayServices()
-
-  const googleUser = await (async () => {
-    try {
-      return await GoogleSignin.getTokens()
-    } catch (error) {
-      return await GoogleSignin.signInSilently()
-    }
-  })()
-  
-  const tokeninfo = await checkTokenExpiry(googleUser.idToken)
-
-  return {
-    token: googleUser.idToken,
-    expires_at: tokeninfo.exp,
-  }
-}
-
-export const handleGoogleRefresh = async () => {
-  try {
-    const response = await promiseRetry((retry, attempts) => {
-      try {
-        return _handleGoogleRefresh()
-      } catch (nextError) {
-        GoogleSignin.signOut()
-        retry(nextError)
-      }
-    }, { retries: 3 })
-
-    return response
-  } catch (error) {
-    Logger.withScope(scope => {
-      scope.setExtra('message', error.message)
-      Logger.captureMessage('FEDERATED_GOOGLE_REFRESH_FAILURE')
-    })
-    throw error
-  }
-}
-
-/**
- * 
- */
-const _federatedGoogleSignin = async () => {
-  await GoogleSignin.hasPlayServices()
-
-  const googleUser = await (async () => {
-    return (
-      await GoogleSignin.getCurrentUser() ||
-      await GoogleSignin.signIn()
-    )
-  })()
-
-  const tokeninfo = await checkTokenExpiry(googleUser.idToken)
-
-  return {
-    token: googleUser.idToken,
-    expires_at: tokeninfo.exp,
-    user: googleUser.user,
-  }
-}
-
-export const federatedGoogleSignin = async () => {
-  try {
-    const response = await promiseRetry((retry, attempts) => {
-      try {
-        return _federatedGoogleSignin()
-      } catch (nextError) {
-        return retry(nextError)
-      }
-    }, { retries: 3 })
-
-    return response
-  } catch (error) {
-    Logger.withScope(scope => {
-      scope.setExtra('message', error.message)
-      Logger.captureMessage('FEDERATED_GOOGLE_SIGNIN_FAILURE')
-    })
-    throw error
-  }
-}
-
-export const federatedGoogleSignout = async () => {
-  await GoogleSignin.revokeAccess()
-  await GoogleSignin.signOut()
-}
+export const federatedGoogleSignin = Google.signin
+export const federatedGoogleSignout = Google.signout
---
+++ src/services/Google.js
@@ -0,0 +1,102 @@
+import { GoogleSignin, statusCodes } from '@react-native-community/google-signin'
+import Config from 'react-native-config'
+
+class TokenExpiredError extends Error {
+  constructor(...args) {
+    super(...args)
+    this.code = 'TOKEN_EXPIRED'
+    Error.captureStackTrace(this, TokenExpiredError)
+  }
+}
+
+GoogleSignin.configure({
+  offlineAccess: false,
+  iosClientId: Config.GOOGLE_SIGNIN_IOS_CLIENT_ID,
+  webClientId: Config.GOOGLE_SIGNIN_ANDROID_CLIENT_ID,
+})
+
+const checkTokenExpiry = async (idToken) => {
+  const data = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`)
+  const response = await data.json()
+  
+  if (!response || response.error) {
+    throw new TokenExpiredError('Token expired')
+  }
+
+  return response
+}
+
+export const signin = async () => {
+  try {
+    await GoogleSignin.hasPlayServices()
+    const userInfo = await GoogleSignin.signIn()
+    const tokeninfo = await checkTokenExpiry(userInfo.idToken)
+
+    return {
+      token: userInfo.idToken,
+      expires_at: tokeninfo.exp,
+      user: userInfo.user,
+    }
+  } catch (error) {
+    if (error.code === 'TOKEN_EXPIRED') {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'TOKEN_EXPIRED')
+        Logger.captureMessage('GOOGLE_SIGNIN')
+      })
+    } else if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'SIGN_IN_CANCELLED')
+        Logger.captureMessage('GOOGLE_SIGNIN')
+      })
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'IN_PROGRESS')
+        Logger.captureMessage('GOOGLE_SIGNIN')
+      })
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'PLAY_SERVICES_NOT_AVAILABLE')
+        Logger.captureMessage('GOOGLE_SIGNIN')
+      })
+    } else {
+      Logger.captureException(error)
+    }
+
+    throw error
+  }
+}
+
+export const refresh = async () => {
+  try {
+    await GoogleSignin.hasPlayServices()
+    const userInfo = await GoogleSignin.signInSilently()
+    const tokeninfo = await checkTokenExpiry(userInfo.idToken)
+
+    return {
+      token: userInfo.idToken,
+      expires_at: tokeninfo.exp,
+      user: userInfo.user,
+    }
+  } catch (error) {
+    if (error.code === 'TOKEN_EXPIRED') {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'TOKEN_EXPIRED')
+        Logger.captureMessage('GOOGLE_REFRESH')
+      })
+    } else if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+      Logger.withScope(scope => {
+        scope.setExtra('code', 'SIGN_IN_REQUIRED')
+        Logger.captureMessage('GOOGLE_REFRESH')
+      })
+    } else {
+      Logger.captureException(error)
+    }
+
+    throw error
+  }
+}
+
+export const signout = async () => {
+  await GoogleSignin.revokeAccess()
+  await GoogleSignin.signOut()
+}
