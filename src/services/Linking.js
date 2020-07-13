0568d1dc48e174f090d6feab212bf67b450d2599
Azim Gadzhiagayev <me@azimgd.com>
2020-07-13 15:51:12 +0500

    Add Likes and Views linking handler;

--- src/services/Linking.js
+++ src/services/Linking.js
@@ -3,18 +3,34 @@ import * as navigationActions from 'navigation/actions'
 import urlPattern from 'url-pattern'
 
 const options = { segmentValueCharset: '/:a-zA-Z0-9_-' }
-const matchComment = new urlPattern(
+const matchComments = new urlPattern(
   '*/user/:userId/post/:postId/comments',
   options
 )
 
+const matchViews = new urlPattern(
+  '*/user/:userId/post/:postId/views',
+  options
+)
+
+const matchLikes = new urlPattern(
+  '*/user/:userId/post/:postId/likes',
+  options
+)
+
 export const deeplinkNavigation = (navigation) => (action) => {
-  const matchedComment = matchComment.match(action)
+  const matchedComments = matchComments.match(action)
+  const matchedViews = matchViews.match(action)
+  const matchedLikes = matchLikes.match(action)
 
   if (action === 'https://real.app/chat/') {
     navigationActions.navigateChat(navigation)()
-  } else if (matchedComment) {
-    navigationActions.navigateComments(navigation, matchedComment)()
+  } else if (matchedComments) {
+    navigationActions.navigateComments(navigation, matchedComments)()
+  } else if (matchedViews) {
+    navigationActions.navigatePostViews(navigation, matchedViews)()
+  } else if (matchedLikes) {
+    navigationActions.navigatePostViews(navigation, matchedLikes)()
   } else {
     Linking.openURL(action)
   }
