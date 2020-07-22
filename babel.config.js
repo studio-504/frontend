da21c79109fd15d5c6f3bb6c29fb2373d357a2db
Azim Gadzhiagayev <me@azimgd.com>
2020-07-22 15:15:34 +0500

    Second round of dependencies update;

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv'
+    'module:metro-react-native-babel-preset'
   ],
   plugins: [
+    ["inline-dotenv"],
     ['module-resolver', {
       root: ['./src'],
       alias: {
