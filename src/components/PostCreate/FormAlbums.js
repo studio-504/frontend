8225e617b3d8747d3076a9c9b110b1cc632cab0d
Alex Omelnitckii <omelniz@gmail.com>
2020-08-25 19:47:15 +0300

    [Trello 34] Newer albums should be the leftmost in the “Create Post” / “Edit Post” screens. (#84)
    * Sort albums by created date
    
    * Fetch reverced albums list by default
    
    * Fix linters errors

--- src/components/PostCreate/FormAlbums.js
+++ src/components/PostCreate/FormAlbums.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native'
+import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 import { Text, Caption } from 'react-native-paper'
 import pathOr from 'ramda/src/pathOr'
 import * as navigationActions from 'navigation/actions'
@@ -13,14 +8,11 @@ import * as navigationActions from 'navigation/actions'
 import { withTheme } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
 import { withTranslation } from 'react-i18next'
+import testIDs from './test-ids'
 
-const FormAlbums = ({
-  t,
-  theme,
-  values,
-  setFieldValue,
-  albumsGet,
-}) => {
+const getAlbums = pathOr([], ['data'])
+
+const FormAlbums = ({ t, theme, values, setFieldValue, albumsGet }) => {
   const styling = styles(theme)
   const navigation = useNavigation()
 
@@ -31,17 +23,27 @@ const FormAlbums = ({
   return (
     <View style={styling.root}>
       <ScrollView style={styling.albums} horizontal>
-        <TouchableOpacity style={[styling.album, styling.albumCreate]} onPress={navigationActions.navigateAlbumCreate(navigation)}>
+        <TouchableOpacity
+          testID={testIDs.albums.newAlbumBtn}
+          style={[styling.album, styling.albumCreate]}
+          onPress={navigationActions.navigateAlbumCreate(navigation)}
+        >
           <Text>{t('New Album')}</Text>
         </TouchableOpacity>
 
-        {pathOr([], ['data'])(albumsGet).map((album, key) => {
-          const style = (values.albumId === album.albumId) ?
-            [styling.album, styling.albumSelected] :
-            [styling.album, styling.albumDefault]
+        {getAlbums(albumsGet).map((album, key) => {
+          const style =
+            values.albumId === album.albumId
+              ? [styling.album, styling.albumSelected]
+              : [styling.album, styling.albumDefault]
 
           return (
-            <TouchableOpacity style={style} onPress={handleAlbumPress(album.albumId)} key={key}>
+            <TouchableOpacity
+              testID={testIDs.albums.item}
+              style={style}
+              onPress={handleAlbumPress(album.albumId)}
+              key={key}
+            >
               <Text>{album.name}</Text>
             </TouchableOpacity>
           )
@@ -49,46 +51,58 @@ const FormAlbums = ({
       </ScrollView>
 
       <TouchableOpacity onPress={navigationActions.navigateAlbums(navigation)}>
-        <Caption>{t('Choose an album from the list to group your posts')}, {t('or click here to browse all your albums')}</Caption>
+        <Caption>
+          {t('Choose an album from the list to group your posts')}, {t('or click here to browse all your albums')}
+        </Caption>
       </TouchableOpacity>
     </View>
   )
 }
 
-const styles = theme => StyleSheet.create({
-  root: {
-  },
-  text: {
-  },
-  albums: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-  },
-  album: {
-    borderRadius: 4,
-    marginRight: theme.spacing.base,
-    marginBottom: 6,
-    padding: 6,
-  },
-  albumCreate: {
-    borderColor: theme.colors.border,
-    borderWidth: 1,
-
-  },
-  albumSelected: {
-    backgroundColor: theme.colors.primary,
-  },
-  albumDefault: {
-    backgroundColor: theme.colors.backgroundSecondary,
-  },
-})
+const styles = (theme) =>
+  StyleSheet.create({
+    root: {},
+    text: {},
+    albums: {
+      flexWrap: 'wrap',
+      flexDirection: 'row',
+    },
+    album: {
+      borderRadius: 4,
+      marginRight: theme.spacing.base,
+      marginBottom: 6,
+      padding: 6,
+    },
+    albumCreate: {
+      borderColor: theme.colors.border,
+      borderWidth: 1,
+    },
+    albumSelected: {
+      backgroundColor: theme.colors.primary,
+    },
+    albumDefault: {
+      backgroundColor: theme.colors.backgroundSecondary,
+    },
+  })
 
 FormAlbums.propTypes = {
   theme: PropTypes.any,
-  values: PropTypes.any,
+  values: PropTypes.shape({ albumId: PropTypes.string }),
   setFieldValue: PropTypes.any,
   t: PropTypes.any,
-  albumsGet: PropTypes.any,
+  albumsGet: PropTypes.shape({
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        createdAt: PropTypes.string.isRequired,
+        albumId: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      }),
+    ),
+  }),
+}
+
+FormAlbums.defaultProps = {
+  values: {},
 }
 
 export default withTranslation()(withTheme(FormAlbums))
---
+++ src/components/PostCreate/FormAlbums.spec.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { renderWithProviders } from 'utils/tests'
+import FormAlbums from './FormAlbums'
+import testIDs from './test-ids'
+
+jest.mock('@react-navigation/native', () => ({ useNavigation: jest.fn() }))
+
+const albums = [
+  {
+    createdAt: '2020-06-25T08:39:16.916487Z',
+    albumId: '1',
+    name: 'Name 1',
+  },
+  {
+    createdAt: '2020-07-25T08:39:16.916487Z',
+    albumId: '2',
+    name: 'Name 2',
+  },
+  {
+    createdAt: '2020-08-25T08:39:16.916487Z',
+    albumId: '3',
+    name: 'Name 3',
+  },
+]
+
+const setup = (props) => renderWithProviders(<FormAlbums setFieldValue={jest.fn()} {...props} />)
+
+describe('FormAlbums', () => {
+  it('should represents only New Album button when albums list empty', () => {
+    const { getByTestId, queryAllByTestId } = setup()
+
+    expect(getByTestId(testIDs.albums.newAlbumBtn)).toBeTruthy()
+    expect(queryAllByTestId(testIDs.albums.item)).toHaveLength(0)
+  })
+
+  it('should represent albums list', () => {
+    const { getByTestId, queryAllByTestId } = setup({ albumsGet: { data: albums } })
+
+    const albumsEls = queryAllByTestId(testIDs.albums.item)
+
+    expect(getByTestId(testIDs.albums.newAlbumBtn)).toBeTruthy()
+    expect(albumsEls).toHaveLength(albums.length)
+
+    albums.forEach((item, index) => {
+      expect(albumsEls[index]).toHaveTextContent(item.name)
+    })
+  })
+})
