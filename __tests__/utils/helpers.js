ceb44c3a82d98c9fb51e6b1d573d0e4d2d08b812
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-09-23 14:36:35 +0300

    Add testAsRootSaga helper

--- __tests__/utils/helpers.js
+++ __tests__/utils/helpers.js
@@ -1,1 +1,9 @@
-export const sleep = () => new Promise(r => setTimeout(r, 0))
+import { all } from 'redux-saga/effects'
+
+export const sleep = () => new Promise((r) => setTimeout(r, 0))
+
+export function testAsRootSaga(saga) {
+  return function* pseudoRootSaga() {
+    yield all(saga())
+  }
+}
