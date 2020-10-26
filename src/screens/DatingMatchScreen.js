70f21e52f558a07e8beca8c3b6b2fc4d8cc6bde7
Azim Gadzhiagayev <me@azimgd.com>
2020-10-26 12:14:42 +0500

    Location permissions check on dating page;

--- src/screens/DatingMatchScreen.js
+++ src/screens/DatingMatchScreen.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import DatingMatchComponent from 'components/DatingMatch'
 import DatingMatchServiceComponent from 'components/DatingMatch/index.service'
+import PermissionsServiceComponent from 'components/Permissions/index.service'
 
 class DatingMatchScreen extends React.Component {
   render() {
     return (
-      <DatingMatchServiceComponent>
-        {(datingMatchProps) => (
-          <DatingMatchComponent
-            {...datingMatchProps}
-          />
+      <PermissionsServiceComponent location>
+        {() => (
+          <DatingMatchServiceComponent>
+            {(datingMatchProps) => (
+              <DatingMatchComponent
+                {...datingMatchProps}
+              />
+            )}
+          </DatingMatchServiceComponent>
         )}
-      </DatingMatchServiceComponent>
+      </PermissionsServiceComponent>
     )
   }
 }
--- src/screens/DatingScreen.js
+++ src/screens/DatingScreen.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import DatingComponent from 'components/Dating'
 import DatingServiceComponent from 'components/Dating/index.service'
+import PermissionsServiceComponent from 'components/Permissions/index.service'
 
 class DatingScreen extends React.Component {
   render() {
     return (
-      <DatingServiceComponent>
-        {(datingProps) => (
-          <DatingComponent
-            {...datingProps}
-          />
+      <PermissionsServiceComponent location>
+        {() => (
+          <DatingServiceComponent>
+            {(datingProps) => (
+              <DatingComponent
+                {...datingProps}
+              />
+            )}
+          </DatingServiceComponent>
         )}
-      </DatingServiceComponent>
+      </PermissionsServiceComponent>
     )
   }
 }
