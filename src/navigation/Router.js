7c74bf3fa032a63851cd5c321277433e5ea977cc
Azim Gadzhiagayev <me@azimgd.com>
2020-11-03 20:52:16 +0500

    Testflight articats; Bugfix Peermissions component; Remove obsoleete loading spinneers;

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -3,7 +3,6 @@ import AuthNavigator from 'navigation/AuthNavigator'
 import AppNavigator from 'navigation/AppNavigator'
 import * as navigationOptions from 'navigation/options'
 import { createStackNavigator } from '@react-navigation/stack'
-import LoadingComponent from 'components/Loading'
 import NetworkComponent from 'components/Network'
 import PinchZoomComponent from 'components/Feed/PinchZoom'
 import FeedContextComponent from 'components/Feed/Context'
@@ -18,11 +17,6 @@ const Router = () => {
       <PinchZoomComponent />
       <NetworkComponent />
       <Stack.Navigator>
-        <Stack.Screen
-          name="Loading"
-          component={LoadingComponent}
-          {...navigationOptions.stackScreenModalStaticProps}
-        />
         <Stack.Screen
           name="App"
           component={AppNavigator}
