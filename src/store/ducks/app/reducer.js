07bc40e1f1d43823a3f3d52be27a289bff13375b
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-11-13 14:54:37 +0300

    Remove user from app reducer

--- src/store/ducks/app/reducer.js
+++ src/store/ducks/app/reducer.js
@@ -30,9 +30,6 @@ const appReadyRequest = (state) => update(state, {
 })
 
 const appReadySuccess = (state, action) => update(state, {
-  user: {
-    $set: action.payload.data,
-  },
   appReady: {
     data: { $set: action.payload.data },
     status: { $set: 'success' },
