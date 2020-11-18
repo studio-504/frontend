409b7f0a5e95edc5de25438a13114b6c73747bed
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-11-18 20:14:55 +0300

    Fix posts loader animation

--- src/components/Feed/PostsLoading.js
+++ src/components/Feed/PostsLoading.js
@@ -1,27 +1,33 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  StyleSheet,
-  View,
-} from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import GridComponent from 'templates/Grid'
 import GridItemComponent from 'templates/GridItem'
 import * as Animatable from 'react-native-animatable'
-
 import { withTheme } from 'react-native-paper'
 
-const PostsGrid = ({
-  theme,
-}) => {
-  const styling = styles(theme)
-  const ref = useRef()
+const fadeInOut = {
+  from: {
+    opacity: 0.8,
+  },
+  to: {
+    opacity: 0.4,
+  },
+}
 
-  useEffect(() => {
-    ref.current.transitionTo({ opacity: 0.2 })
-  }, [])
-  
+const PostsGrid = ({ theme }) => {
+  const styling = styles(theme)
+
   return (
-    <Animatable.View style={styling.root} ref={ref}>
+    <Animatable.View
+      animation={fadeInOut}
+      duration={1200}
+      easing="ease-in-out"
+      iterationCount="infinite"
+      direction="alternate-reverse"
+      style={styling.root}
+      useNativeDriver
+    >
       <GridComponent items={[{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]}>
         {() => (
           <GridItemComponent>
@@ -32,27 +38,23 @@ const PostsGrid = ({
     </Animatable.View>
   )
 }
-const styles = theme => StyleSheet.create({
-  root: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  item: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: theme.colors.disabled,
-    opacity: 0.8,
-  },
-})
-
-PostsGrid.defaultProps = {
-  postsGet: {},
-}
+const styles = (theme) =>
+  StyleSheet.create({
+    root: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    item: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: theme.colors.disabled,
+      opacity: 0.8,
+    },
+  })
 
 PostsGrid.propTypes = {
   theme: PropTypes.any,
-  postsGet: PropTypes.any,
 }
 
 export default withTheme(PostsGrid)
