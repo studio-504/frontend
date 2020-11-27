9ddb741de7bbc7c0a9d6576149ff9a25d6553bae
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-11-27 16:26:29 +0300

    Removed autoFocus

--- src/components/AuthSigninPhone/Form.js
+++ src/components/AuthSigninPhone/Form.js
@@ -42,7 +42,6 @@ const SigninForm = ({
           name="phone"
           component={PhoneField}
           placeholder={t('Phone Number')}
-          autoFocus
         />
       </View>
       <View style={styles.input}>
