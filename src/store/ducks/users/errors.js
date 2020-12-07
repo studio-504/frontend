ce1dad360426dd36672bb5f185923c3db3b061ea
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-12-07 21:34:18 +0300

    Show specific error message for dating enable request

--- src/store/ducks/users/errors.js
+++ src/store/ducks/users/errors.js
@@ -1,8 +1,9 @@
+import propOr from 'ramda/src/propOr'
 import * as constants from 'store/ducks/users/constants'
 
 const messageCodes = {
   /**
-   * 
+   *
    */
   [constants.USERS_EDIT_PROFILE_SUCCESS]: {
     GENERIC: {
@@ -37,12 +38,83 @@ const messageCodes = {
       code: 'GENERIC',
       text: 'Unable to set user dating status',
     },
+    MISSING_FULL_NAME: {
+      code: 'MISSING_FULL_NAME',
+      text: 'Fullname is missing',
+    },
+    MISSING_PHOTO_POST_ID: {
+      code: 'MISSING_PHOTO_POST_ID',
+      text: 'Please, upload a profile photo',
+    },
+    MISSING_AGE: {
+      code: 'MISSING_AGE',
+      text: 'Age is missing',
+    },
+    MISSING_GENDER: {
+      code: 'MISSING_GENDER',
+      text: 'Gender is missing',
+    },
+    MISSING_LOCATION: {
+      code: 'MISSING_LOCATION',
+      text: 'Location is missing',
+    },
+    MISSING_HEIGHT: {
+      code: 'MISSING_HEIGHT',
+      text: 'Height is missing',
+    },
+    MISSING_MATCH_AGE_RANGE: {
+      code: 'MISSING_MATCH_AGE_RANGE',
+      text: 'Match age is missing',
+    },
+    MISSING_MATCH_GENDERS: {
+      code: 'MISSING_MATCH_GENDERS',
+      text: 'Match gender is missing',
+    },
+    MISSING_MATCH_HEIGHT_RANGE: {
+      code: 'MISSING_MATCH_HEIGHT_RANGE',
+      text: 'Match height is missing',
+    },
+    MISSING_MATCH_LOCATION_RADIUS: {
+      code: 'MISSING_MATCH_LOCATION_RADIUS',
+      text: 'Match location radius is missing',
+    },
+    WRONG_AGE_MIN: {
+      code: 'WRONG_AGE_MIN',
+      text: 'Wrong min age',
+    },
+    WRONG_AGE_MAX: {
+      code: 'WRONG_AGE_MAX',
+      text: 'Wrong max age',
+    },
+    WRONG_THREE_HOUR_PERIOD: {
+      code: 'WRONG_THREE_HOUR_PERIOD',
+      text: 'You can change dating status once per 3 hours',
+    },
   },
 }
 
+const getListOf = propOr([])
+
+export const getGraphqlErrorMessage = (key, graphqlError) => {
+  for (let error of getListOf('errors', graphqlError)) {
+    for (let errorCode of getListOf('errorInfo', error)) {
+      if (typeof errorCode === 'string') {
+        const message = getMessagePayload(key, errorCode)
+        const isValidMessage = message.code && message.text
+
+        if (isValidMessage) {
+          return message
+        }
+      }
+    }
+  }
+
+  return getMessagePayload(key, 'GENERIC')
+}
+
 export const getMessagePayload = (key, status = 'GENERIC', nativeError = '') => {
-  return ({
+  return {
     ...messageCodes[key][status],
     nativeError,
-  })
+  }
 }
