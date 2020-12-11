8cb5b9b8ef2673f53b06f54e23b0b6ae85943435
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-12-11 18:29:45 +0300

    Prevent FEED_CARDS_UNSUPPORTED_CARD error

--- src/services/Errors.js
+++ src/services/Errors.js
@@ -1,10 +1,18 @@
 const CODES = {
   CANCEL_REQUEST_ON_SIGNOUT_ERROR: 'CANCEL_REQUEST_ON_SIGNOUT_ERROR',
+  NOT_SUPPORTED_IN_APP_CARD_ERROR: 'NOT_SUPPORTED_IN_APP_CARD_ERROR',
 }
 
 export class CancelRequestOnSignoutError extends Error {
   constructor(...args) {
     super(...args)
     this.code = CODES.CANCEL_REQUEST_ON_SIGNOUT_ERROR
+  }
+}
+
+export class NotSupportedInAppCardError extends Error {
+  constructor(...args) {
+    super(...args)
+    this.code = CODES.NOT_SUPPORTED_IN_APP_CARD_ERROR
   }
 }
