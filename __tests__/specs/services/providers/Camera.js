a8b07b1ef07df460b02b8709d175d21d14fb81a0
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-12-14 22:32:53 +0300

    Prevent choosing multiply photos from library for Profile photo

--- __tests__/specs/services/providers/Camera.js
+++
@@ -1,9 +0,0 @@
-import * as CameraService from 'services/providers/Camera'
-
-describe('Camera provider', () => {
-  it('generates correct asset', () => {
-    expect(CameraService.generateAssetFormat('.heic')).toEqual('HEIC')
-    expect(CameraService.generateAssetFormat('file.jpeg')).toEqual('JPEG')
-    expect(CameraService.generateAssetFormat('JPG')).toEqual('JPEG')
-  })
-})
