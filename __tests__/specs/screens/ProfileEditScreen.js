b722848e4b497f8fe2833e9dfa98cb0337c681da
Alex Omelnitckii <omelniz@gmail.com>
2020-12-15 19:58:12 +0300

    Fix edit profile form (#242)

---
+++ __tests__/specs/screens/ProfileEditScreen.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ProfileEditScreen from 'screens/ProfileEditScreen'
+import { renderWithStore, fireEvent, act } from 'tests/utils'
+import { testField } from 'tests/utils/helpers'
+import * as authSelector from 'store/ducks/auth/selectors'
+
+const user = {
+  email: 'valid@mail.com',
+  username: 'username',
+  fullName: 'fullName',
+  bio: 'bio',
+  phoneNumber: '12312312',
+}
+
+jest.spyOn(authSelector, 'authUserSelector').mockReturnValue(user)
+
+const setup = () => renderWithStore(<ProfileEditScreen />)
+
+describe('ProfileEditScreen', () => {
+  describe('Form', () => {
+    it('have all necessary fields', () => {
+      const { getByLabelText } = setup()
+
+      testField(getByLabelText('email'), { name: 'email' })
+      testField(getByLabelText('username'), { name: 'username' })
+      testField(getByLabelText('fullName'), { name: 'fullName' })
+      testField(getByLabelText('bio'), { name: 'bio' })
+      testField(getByLabelText('phone'), { name: 'phoneNumber' })
+    })
+
+    it('submit button enable by default', () => {
+      const { getByText } = setup()
+
+      expect(getByText('Update')).toBeEnabled()
+    })
+
+    it('initial values', () => {
+      const { getByLabelText } = setup()
+
+      testField(getByLabelText('email'), { value: user.email })
+      testField(getByLabelText('username'), { value: user.username })
+      testField(getByLabelText('fullName'), { value: user.fullName })
+      testField(getByLabelText('bio'), { value: user.bio })
+      testField(getByLabelText('phone'), { value: user.phoneNumber })
+    })
+
+    it('submitting state', async () => {
+      const { getByText } = setup()
+
+      await act(async () => {
+        fireEvent.press(getByText('Update'))
+      })
+
+      expect(getByText('Update')).toBeDisabled()
+    })
+  })
+})
--- src/screens/ProfileEditScreen.js
+++ src/screens/ProfileEditScreen.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import ProfileEditComponent from 'components/ProfileEdit'
 import ProfileEditServiceComponent from 'components/ProfileEdit/index.service'
-import PrivacyServiceComponent from 'components/Privacy/index.service'
 
 class ProfileScreen extends React.Component {
   render() {
     return (
       <ProfileEditServiceComponent>
         {(profileEditProps) => (
-          <PrivacyServiceComponent>
-            {(privacyProps) => (
-              <ProfileEditComponent
-                {...profileEditProps}
-                {...privacyProps}
-              />
-            )}
-          </PrivacyServiceComponent>
+          <ProfileEditComponent
+            {...profileEditProps}
+          />
         )}
       </ProfileEditServiceComponent>
     )
