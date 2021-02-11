ebad74a7bb4dea532ef680974ce32729c2325b67
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-02-11 23:57:10 +0300

    Send subscription errors to Sentry

--- src/store/ducks/subscriptions/saga/index.js
+++ src/store/ducks/subscriptions/saga/index.js
@@ -1,15 +1,14 @@
-import { takeEvery, call, spawn } from 'redux-saga/effects'
-import * as constants from 'store/ducks/subscriptions/constants'
-import subscriptionNotificationStart from 'store/ducks/subscriptions/saga/notifications'
+import { call, spawn } from 'redux-saga/effects'
+import notificationSubscription from 'store/ducks/subscriptions/saga/notifications'
 import chatMessageSubscription from 'store/ducks/subscriptions/saga/chat'
 import cardSubscription from 'store/ducks/subscriptions/saga/card'
-import subscriptionPollStart from 'store/ducks/subscriptions/saga/poll'
+import pollSubscription from 'store/ducks/subscriptions/saga/poll'
 import navigationSubscription from 'store/ducks/subscriptions/saga/navigation'
 
 export default () => [
-  call(subscriptionNotificationStart),
+  call(notificationSubscription),
   call(chatMessageSubscription),
   call(cardSubscription),
-  takeEvery(constants.SUBSCRIPTIONS_POLL_REQUEST, subscriptionPollStart),
+  call(pollSubscription),
   spawn(navigationSubscription),
 ]
