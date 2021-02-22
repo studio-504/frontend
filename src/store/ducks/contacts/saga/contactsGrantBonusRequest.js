e60f6ebc7b15fe82f08367171be4b7980997b007
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-02-22 18:47:19 +0300

    Make grantUserSubscriptionBonus supports FREE_FOR_LIFE code

--- src/store/ducks/contacts/saga/contactsGrantBonusRequest.js
+++ src/store/ducks/contacts/saga/contactsGrantBonusRequest.js
@@ -1,27 +1,51 @@
-import { put, call, select } from 'redux-saga/effects'
+import { put, call, select, takeEvery } from 'redux-saga/effects'
 import * as usersActions from 'store/ducks/users/actions'
+import * as constants from 'store/ducks/contacts/constants'
 import * as selectors from 'store/ducks/contacts/selectors'
+import * as actions from 'store/ducks/contacts/actions'
 import * as queries from 'store/ducks/contacts/queries'
 import * as queryService from 'services/Query'
-import * as contactsConstants from 'store/ducks/contacts/constants'
 import * as navigationActions from 'navigation/actions'
 import * as NavigationService from 'services/Navigation'
+import * as UserService from 'services/User'
+import * as authSelector from 'store/ducks/auth/selectors'
 
-function* contactsGrantBonusRequest() {
+function* contactsCheckBonusRequest() {
   try {
+    const user = yield select(authSelector.authUserSelector)
     const { invited } = yield select(selectors.contactsInvite)
 
-    if (Object.keys(invited).length === contactsConstants.CONTACTS_INVITE_LIMIT) {
-      const navigation = yield NavigationService.getNavigation()
-      
-      yield call(queryService.apiRequest, queries.grantUserSubscriptionBonus)
-      yield put(usersActions.usersGetProfileSelfRequest())
+    if (!UserService.isUserAuthorized(user)) return
+    if (UserService.isUserSubscribed(user)) return
 
-      navigationActions.navigateInviteFriendsSuccess(navigation)
+    if (Object.keys(invited).length === constants.CONTACTS_INVITE_LIMIT) {
+      yield put(actions.contactsGrantBonusRequest({ grantCode: 'FREE_FOR_LIFE' }))
     }
   } catch (error) {
-    // ignore
+    yield put(actions.contactsCheckBonusFailure(error))
   }
 }
 
-export default contactsGrantBonusRequest
+function* contactsGrantBonusRequest(req) {
+  try {
+    yield call(queryService.apiRequest, queries.grantUserSubscriptionBonus, { grantCode: req.payload.grantCode })
+    yield put(actions.contactsGrantBonusSuccess())
+  } catch (error) {
+    yield put(actions.contactsGrantBonusFailure(error))
+  }
+}
+
+function* contactsGrantBonusSuccess() {
+  const navigation = yield NavigationService.getNavigation()
+  yield put(usersActions.usersGetProfileSelfRequest())
+
+  navigationActions.navigateInviteFriendsSuccess(navigation)
+}
+
+export default () => [
+  takeEvery(constants.CONTACTS_GRANT_BONUS_REQUEST, contactsGrantBonusRequest),
+  takeEvery(constants.CONTACTS_GRANT_BONUS_SUCCESS, contactsGrantBonusSuccess),
+  takeEvery(constants.CONTACTS_FOLLOW_SUCCESS, contactsCheckBonusRequest),
+  takeEvery(constants.CONTACTS_INVITE_SUCCESS, contactsCheckBonusRequest),
+  takeEvery(constants.CONTACTS_CHECK_BONUS_REQUEST, contactsCheckBonusRequest),
+]
--- src/store/messages.js
+++ src/store/messages.js
@@ -2,5 +2,6 @@ import authMessages from 'store/ducks/auth/messages'
 import signupMessages from 'store/ducks/signup/messages'
 import usersMessages from 'store/ducks/users/messages'
 import datingMessages from 'store/ducks/dating/messages'
+import contactsMessages from 'store/ducks/contacts/messages'
 
-export default Object.assign(authMessages, signupMessages, usersMessages, datingMessages)
+export default Object.assign(authMessages, signupMessages, usersMessages, datingMessages, contactsMessages)
