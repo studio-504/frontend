6178f623cff9d58cbd72a7d6dda1edbed5e6d93e
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-02-26 20:23:44 +0300

    Use setThemeCode mutation

--- src/store/ducks/themes/queries.js
+++ src/store/ducks/themes/queries.js
@@ -2,7 +2,7 @@ import * as usersSingle from 'store/ducks/users/queries/single'
 
 export const setThemeCode = `
   mutation setThemeCode($themeCode: String!) {
-    setUserDetails(themeCode: $themeCode) {
+    setThemeCode(themeCode: $themeCode) {
       ...singleUserFragment
     }
   }
