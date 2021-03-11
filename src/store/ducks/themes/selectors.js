3abcb3248108c8dc76e68caa436ddcdb0b9b0e17
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-03-11 17:03:37 +0300

    Dark mode popup modal is hard to see

--- src/store/ducks/themes/selectors.js
+++ src/store/ducks/themes/selectors.js
@@ -4,7 +4,9 @@ import themesJson from 'assets/themes.json'
 import * as authSelector from 'store/ducks/auth/selectors'
 import * as constants from 'store/ducks/themes/constants'
 
+export const getTheme = key => (themesJson.find((theme) => theme.key === key) || {}).theme
+
 export const themeSelector = createSelector(authSelector.authUserSelector, (authUser) => {
   const activeTheme = pathOr(constants.DEFAULT_THEME, ['themeCode'])(authUser)
-  return (themesJson.find((theme) => theme.key === activeTheme) || {}).theme
+  return getTheme(activeTheme)
 })
