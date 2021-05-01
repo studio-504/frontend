2a2a34bd63bc4a7fb88fa1f8732845095ed5eeb1
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-05-01 14:27:25 +0300

    refactor: Simplify upload avatar failure flow

--- src/components/ProfilePhotoUpload/index.service.js
+++ src/components/ProfilePhotoUpload/index.service.js
@@ -1,5 +1,4 @@
 import { useEffect } from 'react'
-import { Alert } from 'react-native'
 import * as postsActions from 'store/ducks/posts/actions'
 import * as usersActions from 'store/ducks/users/actions'
 import { useDispatch, useSelector } from 'react-redux'
@@ -19,7 +18,7 @@ const ProfilePhotoUploadComponentService = ({ children }) => {
 
   const postsCreateQueue = useSelector((state) => state.posts.postsCreateQueue)
   const postsCreate = useSelector(postsSelector.postsCreate)
-  const usersEditProfile = useSelector(usersSelector.usersEditProfile)
+  const usersChangeAvatar = useSelector(usersSelector.usersChangeAvatar)
   const cameraCapture = useSelector(state => state.camera.cameraCapture)
 
   const activePhoto = pathOr({}, ['data', 0])(cameraCapture)
@@ -33,28 +32,10 @@ const ProfilePhotoUploadComponentService = ({ children }) => {
     }
   }
 
-  /**
-   * Create a post first and then set photoPostId
-   */
-  const handleUploadSuccess = (postsCreate) => {
-    dispatch(usersActions.usersEditProfileRequest({ photoPostId: postsCreate.payload.postId }))
+  const handleClose = () => {
+    clearProfilePhotoUpload()
+    navigation.goBack()
   }
-
-  const handleUploadFailure = () => {
-    Alert.alert(
-      'Profile Picture Upload Failed',
-      'Please try again',
-      [
-        {
-          text: 'Got it',
-          onPress: handleClose,
-          style: 'cancel',
-        },
-      ],
-      { cancelable: true },
-    )
-  }
-
 
   /**
    * Uploaded photo event listener
@@ -62,10 +43,11 @@ const ProfilePhotoUploadComponentService = ({ children }) => {
    */
    useEffect(() => {
     if (postsCreate.status === 'success') {
-      handleUploadSuccess(postsCreate)
+      dispatch(usersActions.usersChangeAvatarRequest({postId: postsCreate.payload.postId}))
     }
+
     if (postsCreate.status === 'failure') {
-      handleUploadFailure(postsCreate)
+      handleClose()
     }
   }, [postsCreate.status])
 
@@ -85,19 +67,24 @@ const ProfilePhotoUploadComponentService = ({ children }) => {
    * Profile photo change event listener
    * Once photo is uploaded usersEditProfile action must be dispatched with uploaded postId to set profile photo
    */
-  useEffectWhenFocused(() => {
-    if (usersEditProfile.status === 'success') {
-      const backRoute = path(['params', 'backRoute'], route)
 
-      clearProfilePhotoUpload()
+  const usersChangeAvatarSuccess = () => {
+    const backRoute = path(['params', 'backRoute'], route)
 
-      if (backRoute) {
-        navigation.navigate(backRoute)
-      } else {
-        navigation.goBack()
-      }
+    clearProfilePhotoUpload()
+
+    if (backRoute) {
+      navigation.navigate(backRoute)
+    } else {
+      navigation.goBack()
     }
-  }, [usersEditProfile.status])
+  }
+
+  useEffectWhenFocused(() => {
+    if (usersChangeAvatar.status === 'success') {
+      usersChangeAvatarSuccess()
+    }
+  }, [usersChangeAvatar.status])
 
   /**
    * Intended for profile photo upload
@@ -117,16 +104,8 @@ const ProfilePhotoUploadComponentService = ({ children }) => {
     }))
   }, [activePhoto.uri])
 
-  const handleClose = () => {
-    clearProfilePhotoUpload()
-    navigation.goBack()
-  }
-
   return children({
-    usersEditProfile,
     activeUpload,
-    postsCreateQueue,
-    handleClose,
   })
 }
 
