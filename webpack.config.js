0b14d76c66723d695527bcd99aff783a5fdb34fd
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-05-17 19:46:26 +0600

    feat: mock google-signin for web

--- webpack.config.js
+++ webpack.config.js
@@ -101,6 +101,7 @@ module.exports = {
       'react-native-permissions': path.resolve(appDirectory, 'src/web/react-native-permissions'),
       'react-native-circular-progress': path.resolve(appDirectory, 'src/web/react-native-circular-progress'),
       '@invertase/react-native-apple-authentication': path.resolve(appDirectory, 'src/web/react-native-apple-authentication'),
+      '@react-native-community/google-signin': path.resolve(appDirectory, 'src/web/google-signin'),
       '@sentry/react-native': '@sentry/react',
     },
     // If you're working on a multi-platform React Native app, web-specific
