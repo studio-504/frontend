6be11f29a8bfd32ff96bff9c6b94981ed59f1690
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-05-18 11:47:34 +0600

    feat: render app component on web

--- index.web.js
+++ index.web.js
@@ -1,31 +1,13 @@
-import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import { AppRegistry } from 'react-native'
+import App from './App'
+
 import { name as appName } from './app.json'
+import { amplifyConfig } from 'services/AWS'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#142a3d',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#ffffff',
-  },
-})
+amplifyConfig()
+dayjs.extend(relativeTime)
 
 AppRegistry.registerComponent(appName, () => App)
 AppRegistry.runApplication(appName, { rootTag: document.getElementById('react-native-app') })
