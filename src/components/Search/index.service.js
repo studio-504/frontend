d0c9cc0fd788f9a8bf84b4210ae04a14a2052081
Donnie Darko <im@kakajann.com>
2021-06-25 15:17:56 +0500

    remove console.log

--- src/components/Search/index.service.js
+++ src/components/Search/index.service.js
@@ -17,8 +17,6 @@ const SearchService = ({ children }) => {
   const usersGetTrendingUsers = useSelector(usersSelector.usersGetTrendingUsersSelector())
   const postsGetTrendingPosts = useSelector(postsSelector.postsGetTrendingPostsSelector())
 
-  console.log(postsGetTrendingPosts.data)
-
   /**
    * FlatList feed ref, used for scroll to top on tab bar press
    */
