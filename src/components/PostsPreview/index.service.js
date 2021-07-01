a30f112b41c476569cc4634171c0557ba00028a6
Bayram Muhiyev <bay-co@mail.ru>
2021-07-01 15:05:44 +0500

    Show image modal on PostPreview

--- src/components/PostsPreview/index.service.js
+++ src/components/PostsPreview/index.service.js
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import path from 'ramda/src/path'
+import { Platform } from 'react-native'
 
 const PostsPreviewService = ({ children }) => {
   const [selectedPost, setSelectedPost] = useState([])
   const handlePostPress = (post) => {
     setSelectedPost([{
-      url: path(['image', 'url4k'])(post),
+      url: Platform.select({
+        ios: path(['image', 'url4k'])(post),
+        android: `file://${path(['image', 'url4k'])(post)}`,
+      }),
     }])
   }
 
