ff8d7d11b30b37626adc4a2ee2918f046f6a3c34
Bayram Muhiyev <bay-co@mail.ru>
2021-07-02 18:08:42 +0500

    Upgraded missing things from 0.62.0 to 0.63.3

--- ios/REAL/AppDelegate.m
+++ ios/REAL/AppDelegate.m
@@ -11,7 +11,7 @@
 #import <UserNotifications/UserNotifications.h>
 #import <RNCPushNotificationIOS.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -34,7 +34,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
