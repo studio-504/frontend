b253a5a0a95b003e7da66e068632e3b64deaa2d1
Donnie Darko <im@kakajann.com>
2021-07-17 10:59:32 +0500

    detox signup initial tests

--- e2e/specs/auth/sign-up.e2e.js
+++ e2e/specs/auth/sign-up.e2e.js
@@ -1,6 +1,6 @@
 /* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["expect", "toBeVisible", "typeText", "element", "tap", "shouldStayOnStep", "waitForElement"] }] */
 import * as emailHelpers from '../../helpers/email'
-import { permissions } from '../../helpers/users'
+import { credentials, permissions } from '../../helpers/users'
 import { generateString, tap, toBeVisible, toBeNotVisible, typeText, waitForElement } from '../../helpers/utils'
 import { valid } from './data.mock'
 import {
@@ -13,6 +13,7 @@ import {
   AuthPasswordScreen,
   Navigation,
 } from './../../helpers/screens'
+import { describe } from 'jest-circus'
 
 async function getFields() {
   return {
@@ -31,11 +32,16 @@ async function fillField(name, value) {
   await field.typeText(value)
 }
 
+async function testField(field, value, step) {
+  await field.clearText()
+  await field.typeText(value)
+  await shouldStayOnStep(step)
+}
+
 async function shouldStayOnStep(step) {
-  if (step === 'username') {
-    await tap(AuthUsernameScreen.form.submitBtn)
-    await toBeVisible(AuthUsernameScreen.root)
-    await toBeNotVisible(AuthPasswordScreen.root)
+  if (step === 'phone') {
+    await tap(AuthPhoneScreen.form.submitBtn)
+    await toBeVisible(AuthPhoneScreen.root)
   } else if (step === 'password') {
     await tap(AuthPasswordScreen.form.submitBtn)
     await toBeVisible(AuthPasswordScreen.root)
@@ -63,240 +69,287 @@ describe('Feature: Sign up', () => {
   })
 
   describe('As a guest I want to not be able to sign up with incorrect credentials', () => {
-    describe('Rule: Username validation', () => {
-      it('Given: Unauthorized user on signup username step screen', async () => {
+    let email
+    let phone
+
+    beforeAll(async () => {
+      const fields = await getFields()
+      email = fields.email
+      phone = fields.phone
+    })
+
+    describe('Email signup', () => {
+      it('should navigate to signup with email screen', async () => {
         await toBeVisible(AuthHomeScreen.root)
         await tap(AuthHomeScreen.actions.signUpBtn)
-        await toBeVisible(AuthUsernameScreen.root)
-      })
-
-      it('Example: username is a required field', async () => {
-        await shouldStayOnStep('username')
-      })
-
-      it('Example: already exists username', async () => {
-        await fillField('username', 'real')
-        await shouldStayOnStep('username')
-      })
-
-      it('Example: must be at least 3 characters', async () => {
-        await fillField('username', generateString({ length: 2 }))
-        await shouldStayOnStep('username')
-      })
-
-      it('Example: must be at most 30 characters', async () => {
-        await fillField('username', generateString({ length: 31 }))
-        await shouldStayOnStep('username')
-      })
-
-      it('Example: must only contain letters and numbers', async () => {
-        const { username } = await getFields()
-
-        await username.clearText()
-        await username.typeText(valid.username)
-
-        async function testSpecialChar(char) {
-          await username.typeText(char)
-          await shouldStayOnStep('username')
-          await username.tapBackspaceKey()
-        }
-
-        await testSpecialChar('!')
-        await testSpecialChar('&')
-        await testSpecialChar('#')
-        await testSpecialChar('*')
-        await testSpecialChar('@')
-        await testSpecialChar('|')
-      })
-    })
-
-    describe('Rule: Password validation', () => {
-      it('Given: Unauthorized user on signup password step screen', async () => {
-        await fillField('username', valid.username)
-        await tap(AuthUsernameScreen.form.submitBtn)
-        await toBeVisible(AuthPasswordScreen.root)
-      })
-
-      it('Example: password is a required field', async () => {
-        await shouldStayOnStep('password')
-      })
-
-      it('Example: must be at least 8 characters', async () => {
-        await fillField('password', generateString({ length: 7 }))
-        await shouldStayOnStep('password')
-      })
-
-      it('Example: must be at most 50 characters', async () => {
-        await fillField('password', generateString({ length: 51 }))
-        await shouldStayOnStep('password')
-      })
-
-      it('Example: no white space', async () => {
-        await fillField('password', 'value with spaces')
-        await shouldStayOnStep('password')
-      })
-    })
-
-    describe('Rule: Email validation', () => {
-      it('Given: Unauthorized user on signup email step screen', async () => {
-        await fillField('password', valid.password)
-        await tap(AuthPasswordScreen.form.submitBtn)
         await tap(Navigation.authNavigator.signUp.email)
         await toBeVisible(AuthEmailScreen.root)
       })
 
-      it('Example: email is a required field', async () => {
+      it('should fail with invalid email addresses', async () => {
+        await testField(email, 'notvalid', 'email')
+        await testField(email, 'notvalid@', 'email')
+        await testField(email, 'notvalid@email', 'email')
+        await testField(email, 'notvalid@email.', 'email')
         await shouldStayOnStep('email')
       })
 
-      it('Example: must be at least 3 characters', async () => {
-        await fillField('email', generateString({ length: 2 }))
-        await shouldStayOnStep('email')
-      })
-
-      it('Example: must be at most 50 characters', async () => {
-        await fillField('email', generateString({ length: 51 }))
-        await shouldStayOnStep('email')
-      })
-
-      it('Example: must be valid email', async () => {
-        await fillField('email', 'notvalid')
-        await shouldStayOnStep('email')
-
-        await fillField('email', 'notvalid@')
-        await shouldStayOnStep('email')
-
-        await fillField('email', 'notvalid@email')
-        await shouldStayOnStep('email')
-
-        await fillField('email', 'notvalid@email.')
-        await shouldStayOnStep('email')
-      })
-    })
-
-    describe('Rule: Confirmation code validation', () => {
-      it('Given: Unauthorized user on Confirmation code screen', async () => {
-        await fillField('email', valid.email)
+      it('should fail with existing email', async () => {
+        await fillField('email', credentials.email)
         await tap(AuthEmailScreen.form.submitBtn)
-        await toBeVisible(AuthEmailConfirmScreen.root)
-      })
-
-      it('Example: code is a required field', async () => {
-        await shouldStayOnStep('code')
-      })
-
-      it('Example: must be at 6 characters', async () => {
-        await fillField('code', '12345')
-        await shouldStayOnStep('code')
-      })
-
-      it('Example: only numbers', async () => {
-        await fillField('code', '12345a')
-        await shouldStayOnStep('code')
+        await shouldStayOnStep('email')
       })
     })
+
+    describe('Phone signup should fail', () => {
+      it('should navigate to signup with phone screen', async () => {
+        await toBeVisible(AuthEmailScreen.root)
+        await tap(Navigation.authNavigator.signUp.phone)
+        await toBeVisible(AuthPhoneScreen.root)
+      })
+
+      it('should fail with non numeric chars', async () => {
+        await testField(phone, 'abc', 'phone')
+        await testField(phone, '!@#', 'phone')
+
+        await shouldStayOnStep('phone')
+      })
+    })
+
+    // describe('Rule: Username validation', () => {
+    //   it('Given: Unauthorized user on signup username step screen', async () => {
+    //     await toBeVisible(AuthHomeScreen.root)
+    //     await tap(AuthHomeScreen.actions.signUpBtn)
+    //     await toBeVisible(AuthUsernameScreen.root)
+    //   })
+
+    //   it('Example: username is a required field', async () => {
+    //     await shouldStayOnStep('username')
+    //   })
+
+    //   it('Example: already exists username', async () => {
+    //     await fillField('username', 'real')
+    //     await shouldStayOnStep('username')
+    //   })
+
+    //   it('Example: must be at least 3 characters', async () => {
+    //     await fillField('username', generateString({ length: 2 }))
+    //     await shouldStayOnStep('username')
+    //   })
+
+    //   it('Example: must be at most 30 characters', async () => {
+    //     await fillField('username', generateString({ length: 31 }))
+    //     await shouldStayOnStep('username')
+    //   })
+
+    //   it('Example: must only contain letters and numbers', async () => {
+    //     const { username } = await getFields()
+
+    //     await username.clearText()
+    //     await username.typeText(valid.username)
+
+    //     async function testSpecialChar(char) {
+    //       await username.typeText(char)
+    //       await shouldStayOnStep('username')
+    //       await username.tapBackspaceKey()
+    //     }
+
+    //     await testSpecialChar('!')
+    //     await testSpecialChar('&')
+    //     await testSpecialChar('#')
+    //     await testSpecialChar('*')
+    //     await testSpecialChar('@')
+    //     await testSpecialChar('|')
+    //   })
+    // })
+
+    // describe('Rule: Password validation', () => {
+    //   it('Given: Unauthorized user on signup password step screen', async () => {
+    //     await fillField('username', valid.username)
+    //     await tap(AuthUsernameScreen.form.submitBtn)
+    //     await toBeVisible(AuthPasswordScreen.root)
+    //   })
+
+    //   it('Example: password is a required field', async () => {
+    //     await shouldStayOnStep('password')
+    //   })
+
+    //   it('Example: must be at least 8 characters', async () => {
+    //     await fillField('password', generateString({ length: 7 }))
+    //     await shouldStayOnStep('password')
+    //   })
+
+    //   it('Example: must be at most 50 characters', async () => {
+    //     await fillField('password', generateString({ length: 51 }))
+    //     await shouldStayOnStep('password')
+    //   })
+
+    //   it('Example: no white space', async () => {
+    //     await fillField('password', 'value with spaces')
+    //     await shouldStayOnStep('password')
+    //   })
+    // })
+
+    // describe('Rule: Email validation', () => {
+    //   it('Given: Unauthorized user on signup email step screen', async () => {
+    //     await fillField('password', valid.password)
+    //     await tap(AuthPasswordScreen.form.submitBtn)
+    //     await tap(Navigation.authNavigator.signUp.email)
+    //     await toBeVisible(AuthEmailScreen.root)
+    //   })
+
+    //   it('Example: email is a required field', async () => {
+    //     await shouldStayOnStep('email')
+    //   })
+
+    //   it('Example: must be at least 3 characters', async () => {
+    //     await fillField('email', generateString({ length: 2 }))
+    //     await shouldStayOnStep('email')
+    //   })
+
+    //   it('Example: must be at most 50 characters', async () => {
+    //     await fillField('email', generateString({ length: 51 }))
+    //     await shouldStayOnStep('email')
+    //   })
+
+    //   it('Example: must be valid email', async () => {
+    //     await fillField('email', 'notvalid')
+    //     await shouldStayOnStep('email')
+
+    //     await fillField('email', 'notvalid@')
+    //     await shouldStayOnStep('email')
+
+    //     await fillField('email', 'notvalid@email')
+    //     await shouldStayOnStep('email')
+
+    //     await fillField('email', 'notvalid@email.')
+    //     await shouldStayOnStep('email')
+    //   })
+    // })
+
+    // describe('Rule: Confirmation code validation', () => {
+    //   it('Given: Unauthorized user on Confirmation code screen', async () => {
+    //     await fillField('email', valid.email)
+    //     await tap(AuthEmailScreen.form.submitBtn)
+    //     await toBeVisible(AuthEmailConfirmScreen.root)
+    //   })
+
+    //   it('Example: code is a required field', async () => {
+    //     await shouldStayOnStep('code')
+    //   })
+
+    //   it('Example: must be at 6 characters', async () => {
+    //     await fillField('code', '12345')
+    //     await shouldStayOnStep('code')
+    //   })
+
+    //   it('Example: only numbers', async () => {
+    //     await fillField('code', '12345a')
+    //     await shouldStayOnStep('code')
+    //   })
+    // })
   })
 
-  describe('As a guest I want to navigate back through sign up for update credentials', () => {
-    it('Given: Unauthorized user on the last sign up flow step', async () => {
-      await toBeVisible(AuthEmailConfirmScreen.root)
-    })
+  // describe('As a guest I want to navigate back through sign up for update credentials', () => {
+  //   it('Given: Unauthorized user on the last sign up flow step', async () => {
+  //     await toBeVisible(AuthEmailConfirmScreen.root)
+  //   })
 
-    it('When user click header left button #1', async () => {
-      await toBeVisible(AuthEmailConfirmScreen.header.backBtn)
-      await tap(AuthEmailConfirmScreen.header.backBtn)
-    })
+  //   it('When user click header left button #1', async () => {
+  //     await toBeVisible(AuthEmailConfirmScreen.header.backBtn)
+  //     await tap(AuthEmailConfirmScreen.header.backBtn)
+  //   })
 
-    it('Then open email screen with saved value', async () => {
-      await toBeVisible(AuthEmailScreen.root)
+  //   it('Then open email screen with saved value', async () => {
+  //     await toBeVisible(AuthEmailScreen.root)
 
-      const { email } = await getFields()
-      await expect(email).toHaveValue(valid.email)
-    })
+  //     const { email } = await getFields()
+  //     await expect(email).toHaveValue(valid.email)
+  //   })
 
-    it('When user click header left button #2', async () => {
-      await tap(AuthEmailScreen.header.backBtn)
-    })
+  //   it('When user click header left button #2', async () => {
+  //     await tap(AuthEmailScreen.header.backBtn)
+  //   })
 
-    it('Then open password screen with saved value', async () => {
-      await toBeVisible(AuthPasswordScreen.root)
+  //   it('Then open password screen with saved value', async () => {
+  //     await toBeVisible(AuthPasswordScreen.root)
 
-      const { password } = await getFields()
-      await expect(password).toHaveValue('••••••••')
-    })
+  //     const { password } = await getFields()
+  //     await expect(password).toHaveValue('••••••••')
+  //   })
 
-    it('When user click header left button again #3', async () => {
-      await tap(AuthPasswordScreen.header.backBtn)
-    })
+  //   it('When user click header left button again #3', async () => {
+  //     await tap(AuthPasswordScreen.header.backBtn)
+  //   })
 
-    it('Then open username screen with saved value', async () => {
-      await toBeVisible(AuthUsernameScreen.root)
+  //   it('Then open username screen with saved value', async () => {
+  //     await toBeVisible(AuthUsernameScreen.root)
 
-      const { username } = await getFields()
-      await expect(username).toHaveValue(valid.username)
-    })
+  //     const { username } = await getFields()
+  //     await expect(username).toHaveValue(valid.username)
+  //   })
 
-    it('When user click header left button again #4', async () => {
-      await tap(AuthUsernameScreen.header.backBtn)
-    })
+  //   it('When user click header left button again #4', async () => {
+  //     await tap(AuthUsernameScreen.header.backBtn)
+  //   })
 
-    it('Then open sign up home screen', async () => {
-      await toBeVisible(AuthHomeScreen.root)
-    })
-  })
+  //   it('Then open sign up home screen', async () => {
+  //     await toBeVisible(AuthHomeScreen.root)
+  //   })
+  // })
 
-  describe('As a guest I want to successfully sign up with email', () => {
-    it('Given: Unauthorized user on auth home screen', async () => {
-      await toBeVisible(AuthHomeScreen.root)
-    })
+  // describe('As a guest I want to successfully sign up with email', () => {
+  //   it('Given: Unauthorized user on auth home screen', async () => {
+  //     await toBeVisible(AuthHomeScreen.root)
+  //   })
 
-    it('Then click Use Phone or Email button', async () => {
-      await tap(AuthHomeScreen.actions.signUpBtn)
-    })
+  //   it('Then click Use Phone or Email button', async () => {
+  //     await tap(AuthHomeScreen.actions.signUpBtn)
+  //   })
 
-    it('When username step screen opened', async () => {
-      await toBeVisible(AuthUsernameScreen.root)
-    })
+  //   it('When username step screen opened', async () => {
+  //     await toBeVisible(AuthUsernameScreen.root)
+  //   })
 
-    it('Then type valid username and click next', async () => {
-      await typeText(AuthUsernameScreen.form.username, valid.username)
-      await tap(AuthUsernameScreen.form.submitBtn)
-    })
+  //   it('Then type valid username and click next', async () => {
+  //     await typeText(AuthUsernameScreen.form.username, valid.username)
+  //     await tap(AuthUsernameScreen.form.submitBtn)
+  //   })
 
-    it('Then type valid password and click next', async () => {
-      await typeText(AuthPasswordScreen.form.password, valid.password)
-      await tap(AuthPasswordScreen.form.submitBtn)
-    })
+  //   it('Then type valid password and click next', async () => {
+  //     await typeText(AuthPasswordScreen.form.password, valid.password)
+  //     await tap(AuthPasswordScreen.form.submitBtn)
+  //   })
 
-    it('When sign up by phone screen opened', async () => {
-      await toBeVisible(AuthPhoneScreen.root)
-    })
+  //   it('When sign up by phone screen opened', async () => {
+  //     await toBeVisible(AuthPhoneScreen.root)
+  //   })
 
-    it('Then click the email tab', async () => {
-      await tap(Navigation.authNavigator.signUp.email)
-    })
+  //   it('Then click the email tab', async () => {
+  //     await tap(Navigation.authNavigator.signUp.email)
+  //   })
 
-    it('When sign up by email screen opened', async () => {
-      await toBeVisible(AuthEmailScreen.root)
-    })
+  //   it('When sign up by email screen opened', async () => {
+  //     await toBeVisible(AuthEmailScreen.root)
+  //   })
 
-    it('Then type valid email and click next', async () => {
-      await typeText(AuthEmailScreen.form.email, inbox.emailAddress)
-      await tap(AuthEmailScreen.form.submitBtn)
-    })
+  //   it('Then type valid email and click next', async () => {
+  //     await typeText(AuthEmailScreen.form.email, inbox.emailAddress)
+  //     await tap(AuthEmailScreen.form.submitBtn)
+  //   })
 
-    it('When confirm email screen opened', async () => {
-      await toBeVisible(AuthEmailConfirmScreen.root)
-    })
+  //   it('When confirm email screen opened', async () => {
+  //     await toBeVisible(AuthEmailConfirmScreen.root)
+  //   })
 
-    it('Then type received confirmation code', async () => {
-      const confirmationCode = await emailHelpers.extractCodeFromLatestEmail(inbox.id)
-      await typeText(AuthEmailConfirmScreen.form.confirmationCode, confirmationCode)
+  //   it('Then type received confirmation code', async () => {
+  //     const confirmationCode = await emailHelpers.extractCodeFromLatestEmail(inbox.id)
+  //     await typeText(AuthEmailConfirmScreen.form.confirmationCode, confirmationCode)
 
-    })
+  //   })
 
-    it('Then user see feed screen', async () => {
-      await waitForElement(FeedScreen.root)
-    })
-  })
+  //   it('Then user see feed screen', async () => {
+  //     await waitForElement(FeedScreen.root)
+  //   })
+  // })
 })
